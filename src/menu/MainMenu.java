995dfedf8ffc064e5e9cf1e6b97b7cd3fe7048c2
Antonio <burevski.antonio5@gmail.com>
2020-12-17 04:29:24 +0100

    a

---
+++ src/menu/MainMenu.java
@@ -0,0 +1,121 @@
+package menu;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.text.*;
+
+public class MainMenu extends Application {
+
+
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(500, 500);
+
+
+
+        MenuItem itemExit = new MenuItem("EXIT");
+        itemExit.setOnMouseClicked(event -> System.exit(0));
+
+        MenuBox menu = new MenuBox(
+                new MenuItem("PLAY"),
+                new MenuItem("HELP"),
+                itemExit);
+        menu.setTranslateX(60);
+        menu.setTranslateY(110);
+
+        root.getChildren().addAll(menu);
+        return root;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(createContent());
+        primaryStage.setTitle("MainMenu Connect Four");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private static class Title extends StackPane {
+        public Title(String name) {
+            Rectangle bg = new Rectangle(250, 60);
+            bg.setStroke(Color.WHITE);
+            bg.setStrokeWidth(2);
+            bg.setFill(null);
+
+            Text text = new Text(name);
+            text.setFill(Color.BLACK);
+            text.setFont(Font.font("Tw Cen MT Condensed", FontWeight.SEMI_BOLD, 50));
+
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(bg, text);
+        }
+    }
+
+    private static class MenuBox extends VBox {
+        public MenuBox(MenuItem... items) {
+            getChildren().add(createSeparator());
+
+            for (MenuItem item : items) {
+                getChildren().addAll(item, createSeparator());
+            }
+        }
+
+        private Line createSeparator() {
+            Line sep = new Line();
+            sep.setEndX(200);
+            sep.setStroke(Color.DARKGREY);
+            return sep;
+        }
+    }
+
+
+    private static class MenuItem extends StackPane {
+        public MenuItem(String name) {
+            LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[]{
+                    new Stop(0, Color.DARKVIOLET),
+                    new Stop(0.1, Color.BLACK),
+                    new Stop(0.9, Color.BLACK),
+                    new Stop(1, Color.DARKVIOLET)
+            });
+
+            Rectangle bg = new Rectangle(400, 80);
+            bg.setOpacity(0.8);
+
+            Text text = new Text(name);
+            text.setFill(Color.DARKGREY);
+            text.setFont(Font.font("Tw Cen MT Condensed", FontWeight.SEMI_BOLD, 22));
+
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(bg, text);
+
+            setOnMouseEntered(event -> {
+                bg.setFill(gradient);
+                text.setFill(Color.WHITE);
+            });
+
+
+            setOnMouseExited(event -> {
+                bg.setFill(Color.BLACK);
+                text.setFill(Color.DARKGREY);
+            });
+
+            setOnMousePressed(event -> {
+                bg.setFill(Color.DARKVIOLET);
+            });
+
+            setOnMouseReleased(event -> {
+                bg.setFill(gradient);
+            });
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
